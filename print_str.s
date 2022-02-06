7033c46a2771deb9b5706694abcdb9b6590d035c
keagleV <kiarash_sedghi@protonmail.com>
2022-02-06 22:45:51 +0330

    adding hex print

--- print_str.s
+++ print_str.s
@@ -47,8 +47,6 @@ print_str:
 		ret
 
 
-
-
 print_nl:
 
 	; Push AX register
@@ -73,3 +71,97 @@ print_nl:
 	pop ax
 
 	ret
+
+
+print_hex:
+
+	mov bp , sp
+
+	; Save all registers 
+	pusha
+
+	; Reading the wanted value into DX
+	mov dx,[bp+0x02]
+	mov ax,dx
+
+	; Moving 4 bytes to DX and CX registers
+	
+	; right-first 4 bits
+	and al,0x0f
+	mov cl,al
+
+	; right-second 4 bits
+	mov al,dl
+	and al,0xf0
+	shr al,4
+	mov ch,al
+
+
+	; left-first 4 bits
+	mov ah,dh
+	and ah,0x0f
+	mov dl,ah
+
+	; left-second 4 bits
+	mov ah,dh
+	and ah,0xf0
+	shr ah,4
+	mov dh,ah
+
+	call hex_to_char
+
+	popa
+
+	ret
+
+
+hex_to_char:
+	
+	mov ah,0x0e
+
+	add dh,0x30
+	cmp dh,0x39
+
+	jng h1
+		add dh,0x07
+	h1:
+	mov al,dh
+	int 0x10
+
+
+
+
+	add dl,0x30
+	cmp dl,0x39
+
+	jng h2
+		add dl,0x07
+	h2:
+	mov al,dl
+	int 0x10
+
+
+
+	add ch,0x30
+
+	cmp ch,0x39
+
+	jng h3
+		add ch,0x07
+	h3:
+	mov al,ch
+	int 0x10
+
+
+	add cl,0x30
+	cmp cl,0x39
+
+	jng h4
+		add cl,0x07
+	h4:
+	mov al,cl
+	int 0x10
+
+
+	ret 
+
