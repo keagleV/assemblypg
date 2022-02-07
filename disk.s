afd714a4b992ac55094ba1e34b7b10777aaa5315
keagleV <kiarash_sedghi@protonmail.com>
2022-02-07 16:59:51 +0330

    updating disk.s

--- disk.s
+++ disk.s
@@ -11,17 +11,23 @@ load_from_disk:
 	mov bx,0x0000
 
 	mov ch,0x01
-	mov cl,0x01
+	mov cl,0x02
 
 	mov dh,0x00
 
-	mov dl,0x80
+	; mov dl,0x80
 
 
 	int 0x13
+	; jmp $
 
 	jc error_loading
-
+	mov bx,SUCCESS
+	
+	push bx
+	call print_str
+	pop bx
+	
 	jmp end
 
 
@@ -29,9 +35,10 @@ load_from_disk:
 error_loading:
 	
 	mov bx,ERROR
-
-	call print_str
 	
+	push bx
+	call print_str
+	pop bx
 
 end:
 	popa
@@ -39,7 +46,7 @@ end:
 
 
 ERROR: db 'Error loading from disk',0x00
-SUCCESS: db 'Success'
+SUCCESS: db 'Success',0x00
 
 
 
